23467a261e7a28610cf0bc899019d0d1cb3f5612
Robert Tenney <rob10esf@gmail.com>
2018-05-22 15:06:40 -0400

    Implement testing

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import path from 'path';
-import open from 'open';
+// import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
-
+/* eslint-disable no-console */
 const port = 8000;
 const app = express();
 const compiler = webpack(config);
