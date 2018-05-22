4c9041064bf217bb8393af7035a4fd3ba2e17993
Robert Tenney <rob10esf@gmail.com>
2018-05-22 13:20:36 -0400

    Added webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-const port = 3000;
+const port = 8000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  }),
+);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
     console.error(err);
   } else {
-    open('http://localhost:' + port);
+    // open('http://localhost:' + port);
   }
 });
